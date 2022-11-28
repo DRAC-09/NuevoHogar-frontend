06d21e52fbb0d35d5f75a54c8e742e77695637ee
DRAC-09 <allan.estrada9@gmail.com>
2022-11-27 22:09:06 -0600

    Modificado fondo de la foto de perfil del componente ModificarMascota

--- src/Componentes/mascotas/mascota/Card.js
+++ src/Componentes/mascotas/mascota/Card.js
@@ -5,12 +5,7 @@ const mascota = (props) => {
      return (
                <div className='card shadow'>
                     <img src={props.imagen} className='card-img-top'></img>
-                         <h5 className="texto">{props.nombre}</h5>
-
-                         {/* <div className='texto'>Edad: {props.edad}</div> */}
-                         {/* <div className='texto'>Raza: {props.raza}</div> */}
-                         {/* <div className='texto'>Sexo: {props.sexo}</div> */}
-                         {/* <div className='texto'>Descripción: {props.descripcion}</div> */}
+                    <h5 className="texto">{props.nombre}</h5>
                </div>
      )    
 }
