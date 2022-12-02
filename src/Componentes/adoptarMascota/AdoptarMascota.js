74ecec563596be2d7281d2031fed9f78c4422bd6
JiovannyMH <114841637+JiovannyMH@users.noreply.github.com>
2022-12-01 18:21:56 -0600

    actualizacion de pagina adoptar, conectado a la base de datos

--- src/Componentes/adoptarMascota/AdoptarMascota.js
+++ src/Componentes/adoptarMascota/AdoptarMascota.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -49,33 +49,41 @@ function Copyright(props) {
     </Typography>
   );
 }
-export default function Historial() {
-  const [mascotas, setPersons] = useState([
-    {
-      id: 1,
-      nombre: "Kevin",
-      descripcion: "Frontend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar2.png",
-    },
-    {
-      id: 2,
-      nombre: "Alex",
-      descripcion: "Backend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar3.png",
-    },
-    {
-      id: 3,
-      nombre: "Ben",
-      descripcion: "Designer",
-      img: "https://bootdey.com/img/Content/avatar/avatar4.png",
-    },
-    {
-      id: 4,
-      nombre: "David",
-      descripcion: "Designer 1",
-      img: "https://bootdey.com/img/Content/avatar/avatar5.png",
-    },
-  ]);
+const AdoptarMascota = ({handleEvent}) => {
+
+  const [mascotas, setMascotas] = useState([]);
+     const [mascota, setMascota] = useState(' ');
+
+  useEffect(() => {
+    // console.log(mascotas)
+    // Obtenermos las mascotas de la base de datos
+    fetch("https://nuevo-hogar-backend.vercel.app/mascota", {
+         method: 'GET',
+         headers: {
+                   'Accept': 'application/json',
+                   'Content-Type': 'application/json'
+              }
+         })
+         .then(res => res.json())
+         .then(data => {
+              setMascotas(data)
+              // console.log(data)
+         })
+         .catch(res => console.log(res));
+},[]);
+
+const pet = (mascotaDetalles) => {
+  // setMascota(mascotaDetalles)
+  handleEvent(false)
+  localStorage.setItem('mascota', JSON.stringify(mascotaDetalles));
+  // event.handleEvent(true)
+  // document.getElementById('modificar-mascota').style.display = 'block'
+  // document.getElementById('mascotas').style.display = 'none'
+
+  
+  // console.log(mascota)
+}
+
     const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -128,25 +136,25 @@ export default function Historial() {
         <Container sx={{ py: 0  }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={3}>
-            {mascotas.map((mascotas) => (
-              <Grid item key={mascotas.id} xs={12} sm={6} md={3}>
+            {mascotas.map((mascota) => (
+              <Grid item key={mascota.id} xs={12} sm={6} md={3} onClick={ (e) => pet(mascota,e)}>
                 <Card
                   sx={{ maxWidth:345}}
                 >
                   <CardMedia
-                    component="img"
+                    component="imagen"
                     sx={{
                       height:140
                     }}
-                    image = {mascotas.img}
+                    image=        {mascota.imagen}
                     alt="random"
                   />
                   <CardContent sx={{ flexGrow: 1, bgcolor:'black'}} >
-                    <Typography gutterBottom variant="h5" component="h2" color="white">
-                      {mascotas.nombre}
+                    <Typography gutterBottom variant="h6" color="white">
+                      {mascota.nombre}
                     </Typography>
                     <Typography color="white">
-                      {mascotas.descripcion}
+                      {mascota.descripcion}
                     </Typography>
                   </CardContent>
                   <CardActions sx={{bgcolor:'black'}}>
@@ -167,3 +175,5 @@ export default function Historial() {
             </div>
   )
 }
+
+export default AdoptarMascota;
